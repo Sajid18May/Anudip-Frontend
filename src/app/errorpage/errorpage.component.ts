f5081eee44de4298fb5d758d555b5d9c477b39cb
Sajid18May <sajid1852002@gmail.com>
2023-11-27 15:26:15 +0530

    error page

--- src/app/errorpage/errorpage.component.ts
+++ src/app/errorpage/errorpage.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, Input} from '@angular/core';
+declare var anime: any;
 
 @Component({
   selector: 'app-errorpage',
   templateUrl: './errorpage.component.html',
   styleUrl: './errorpage.component.css'
 })
-export class ErrorpageComponent {
 
+export class ErrorpageComponent implements AfterViewInit  {
+
+  ngAfterViewInit(): void {{
+  anime({
+    targets: '.row svg',
+    translateY: 10,
+    autoplay: true,
+    loop: true,
+    easing: 'easeInOutSine',
+    direction: 'alternate'
+  });
+
+  anime({
+    targets: '#zero',
+    translateX: 10,
+    autoplay: true,
+    loop: true,
+    easing: 'easeInOutSine',
+    direction: 'alternate',
+    scale: [{value: 1}, {value: 1.4}, {value: 1, delay: 250}],
+      rotateY: {value: '+=180', delay: 200},
+  });
+
+  }
+ }
 }
