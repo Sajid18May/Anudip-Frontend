5f2821bc44ff1029cadd28c9cfe7cb314eb096b9
Sajid18May <sajid1852002@gmail.com>
2023-11-30 00:52:10 +0530

    Images added for books

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../models/book';
 import { BooksService } from '../services/books.service';
+import { CartService } from '../services/cart.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-books',
@@ -9,12 +11,26 @@ import { BooksService } from '../services/books.service';
 })
 export class BooksComponent implements OnInit {
 
-  Books!: Book[];
-  constructor(private bookService: BooksService) { }
+  Books: Book[]=[];
+  constructor(private bookService: BooksService,private cartService:CartService,private activetedRoute:ActivatedRoute) {
+    // this.activetedRoute.params.subscribe((param)=>{
+    //   if(param['searchTerm'])
+    //   this.Books=this.getBooksBySearchTerm(param['searchTerm']);
+    // })
+
+   }
 
   ngOnInit(): void {
     this.bookService.getBooks().subscribe((data: Book[]) => {
       this.Books = data;
     });
+  }
+  getBooksBySearchTerm(searchTerm:string){
+    return this.bookService.getBooks().subscribe((data: Book[]) => {
+      this.Books = data.filter(book=>book.title.toLowerCase().includes(searchTerm.toLowerCase()));
+    })
+  }
+
+  addtoCart(){
   }
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,9 +1,41 @@
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Cart } from './../models/cart';
 import { Injectable } from '@angular/core';
+import { Book } from '../models/book';
+import { Cartitem } from '../models/cartitem';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-
+  private cart:Cart=new Cart();
+  private cartSubject:BehaviorSubject<Cart>=new BehaviorSubject(this.cart);
   constructor() { }
+
+  addToCart(book:Book):void{
+    let cartItem=this.cart.items.find(item=>item.book.title===book.title);
+    if(cartItem)
+      return;
+    this.cart.items.push(new Cartitem(book));
+  }
+
+  removeFromCart(bookTitle:string):void{
+    this.cart.items=this.cart.items.filter(item=>item.book.title!=bookTitle);
+  }
+
+  changeQuantity(bookTitle:string,quantity:number){
+    let cartitem=this.cart.items.find(item=>item.book.title===bookTitle);
+    if(!cartitem)
+      return;
+    cartitem.quantity=quantity;
+    cartitem.price=quantity*cartitem.book.price;
+  }
+
+  clearCart(){
+    this.cart=new Cart();
+  }
+
+  getCartObservable():Observable<Cart>{
+    return this.cartSubject.asObservable();
+  }
 }
