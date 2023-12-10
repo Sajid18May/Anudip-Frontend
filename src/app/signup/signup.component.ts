639bb61a92fe703f2484696eeea3a63b37c05d19
Sajid18May <sajid1852002@gmail.com>
2023-12-10 13:29:27 +0530

    form validation in sign up page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -10,31 +10,26 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SignupComponent implements OnInit{
   message:any;
-  form:any;
-
- user:User=new User();
- constructor(private service:UserserviceService) {
-  this.form=new FormGroup({
-    first_name:new FormControl('',[
+  user:User=new User();
+  form=new FormGroup({
+    first_name:new FormControl(this.user.first_name,[
       Validators.required,
-      Validators.minLength(5)
+      Validators.minLength(2)
     ]),
-    last_name:new FormControl(''),
-    email:new FormControl(''),
-    password:new FormControl(''),
-    phone_number:new FormControl(''),
-    address:new FormControl('')})
-
-
-  }
- ngOnInit() {
-}
-get first_name(){
-  return this.form.get('first_name');
+    last_name:new FormControl(this.user.last_name,[Validators.required,Validators.minLength(2)]),
+    email:new FormControl(this.user.email,[Validators.required,Validators.email]),
+    password:new FormControl(this.user.password,[Validators.required,Validators.minLength(8)]),
+    phone_number:new FormControl(this.user.phone_number,[Validators.required,]),
+    address:new FormControl(this.user.address,[Validators.required])
+  });
+ constructor(private service:UserserviceService) { }
+ ngOnInit():void { }
+ get f(){
+  return this.form.controls;
 }
 
 public registerNow(){
-  let resp=this.service.doRegistration(this.form.value);
+  let resp=this.service.doRegistration(this.user);
   resp.subscribe((data)=>this.message=data);
 }
 }
