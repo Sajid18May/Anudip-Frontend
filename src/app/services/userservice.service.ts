a5847e2342049c96435411a7134b01e4ebe301ad
Sajid18May <sajid1852002@gmail.com>
2023-12-10 15:23:36 +0530

    do registrations argoment changed from User to any

--- src/app/services/userservice.service.ts
+++ src/app/services/userservice.service.ts
@@ -16,7 +16,7 @@ export class UserserviceService {
     @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
-  public doRegistration(user: User): Observable<any> {
+  public doRegistration(user: any): Observable<any> {
     return this.http.post("http://localhost:8080/addCustomers", user);
   }
 
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -19,7 +19,7 @@ export class SignupComponent implements OnInit{
     last_name:new FormControl(this.user.last_name,[Validators.required,Validators.minLength(2)]),
     email:new FormControl(this.user.email,[Validators.required,Validators.email]),
     password:new FormControl(this.user.password,[Validators.required,Validators.minLength(8)]),
-    phone_number:new FormControl(this.user.phone_number,[Validators.required,]),
+    phone_number:new FormControl(this.user.phone_number,[Validators.required,Validators.pattern('[0-9]{10}')]),
     address:new FormControl(this.user.address,[Validators.required])
   });
  constructor(private service:UserserviceService) { }
@@ -29,7 +29,7 @@ export class SignupComponent implements OnInit{
 }
 
 public registerNow(){
-  let resp=this.service.doRegistration(this.user);
+  let resp=this.service.doRegistration(this.form.value);
   resp.subscribe((data)=>this.message=data);
 }
 }
